94e53fdc670c45cb1fc432129961850b7956c01e
mperkinson <mperkinson@gmail.com>
2025-03-25 11:35:24 -0400

    added player's triangle

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 import constants
+from player import Player
 
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
+
