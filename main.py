44ecd618f5bdc2994d33ba1e1892c51f764b7800
mperkinson <mperkinson@gmail.com>
2025-03-28 12:45:09 -0400

    Starting over

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import pygame
-import constants
-from player import Player
+import constants as consts
 
 
 def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {consts.SCREEN_WIDTH}")
+    print(f"Screen height: {consts.SCREEN_HEIGHT}")
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
-    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
-    dt = 0
+    screen = pygame.display.set_mode((consts.SCREEN_WIDTH, consts.SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
@@ -16,13 +15,8 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
         pygame.display.flip()
-
-        # limit the framerate to 60 FPS
-        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
-
